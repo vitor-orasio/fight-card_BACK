60c9d682e2c528f4ce95600c64e0d46fc76f4f44
Vitor Eduardo <vitornascimento6650@gmail.com>
2026-05-02 21:50:00 -0300

    fetch: create characterJDBC Insert and FindAll

---
+++ src/main/java/model/dao/daoJDBC/CharacterDaoJDBC.java
@@ -0,0 +1,120 @@
+package model.dao.daoJDBC;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import DB.DbConnection;
+import DB.exceptions.DbException;
+import model.dao.CharacterDao;
+import model.entities.Character;
+import model.entities.User;
+
+public class CharacterDaoJDBC implements CharacterDao {
+	private Connection conn;
+	
+	@Override
+	public void insert(Character character) {
+		PreparedStatement st = null;
+		
+		try {
+			st = conn.prepareStatement("INSERT INTO tb_character(user_id, archetype_character, name_character, attack_character, defense_character, ranking_type, img_character)"
+					+ "VALUES (?, ?, ?, ?, ?, ?, ?)", java.sql.Statement.RETURN_GENERATED_KEYS);
+			
+			st.setInt(1, character.getUser().getId());
+			st.setString(2, character.getArchetype());
+			st.setString(3, character.getName());
+			st.setInt(4, character.getAttack());
+			st.setInt(5, character.getDefense());
+			st.setString(6, character.getRanking_type());
+			st.setString(7, character.getImg_perfil());
+			
+			int rowsAffected = st.executeUpdate();
+			
+			if(rowsAffected > 0) {
+				ResultSet rs = st.getGeneratedKeys();
+				
+				if(rs.next()) {
+					int id = rs.getInt(1);
+					character.setId(id);
+				}
+				
+				DbConnection.closeResultSet(rs);
+			} else {
+				throw new DbException("Unexpected error! No rows affected!");
+			}
+		} catch (SQLException e) {
+			throw new DbException(e.getMessage());
+		} finally {
+			DbConnection.closeStatement(st);
+		}
+	}
+	
+	@Override
+	public List<Character> findAll() {
+		PreparedStatement st = null;
+		ResultSet rs = null;
+		
+		try {
+			st = conn.prepareStatement("SELECT * FROM tb_character");
+			
+			rs = st.executeQuery();
+			List<Character> listCharacter = new ArrayList<Character>();
+			
+			while(rs.next()) {
+				Character character = instantiateCharacter(rs);
+				listCharacter.add(character);
+			}
+			
+			return listCharacter;
+		} catch(SQLException e) {
+			throw new DbException(e.getMessage());
+		} finally {
+			DbConnection.closeStatement(st);
+			DbConnection.closeResultSet(rs);
+		}
+	}
+	
+	
+	private Character instantiateCharacter(ResultSet rs) throws SQLException {
+		Character character = new Character();
+		User user = character.getUser();
+		character.setId(rs.getInt("id_character"));
+		character.setUser(user);
+		character.setArchetype(rs.getString("archetype_character"));
+		character.setName(rs.getString("name_character"));
+		character.setAttack(rs.getInt("attack_character"));
+		character.setDefense(rs.getInt("defense_character"));
+		character.setRanking_type(rs.getString("ranking_type"));
+		character.setImg_perfil(rs.getString("img_character"));
+		
+		return character;
+	}
+
+	@Override
+	public void update(Character character) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteById(Integer id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Character characterById(Integer id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public User findByUser(Integer id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
